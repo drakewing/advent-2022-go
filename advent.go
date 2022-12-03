b17140863c6a587258eec0301af37bbce4ef8778
Matt Drake <drake.mattc@gmail.com>
2022-12-02 18:40:52 -0800

    Finish d01p2

--- advent.go
+++ advent.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("day 1, problem 1: %d", d1p1())
+	fmt.Printf("day 1, problem 1: %d\n", d01p1())
+	fmt.Printf("day 1, problem 2: %d\n", d01p2())
 }
--- day1.go
+++ day1.go
@@ -10,6 +10,34 @@ type backpack struct {
 	food []int
 }
 
+func d01p1() int {
+	backpacks := buildBackpacks()
+	maxCals := -1
+
+	for _, bp := range backpacks {
+		calories := sumIntSlice(bp.food)
+		if calories > maxCals {
+			maxCals = calories
+		}
+	}
+
+	return maxCals
+}
+
+func d01p2() int {
+	backpacks := buildBackpacks()
+	maxCals := [3]int{-1, -1, -1}
+
+	for _, bp := range backpacks {
+		calories := sumIntSlice(bp.food)
+		if calories > maxCals[2] {
+			insertIntoFixedSortedSlice(calories, maxCals[:])
+		}
+	}
+
+	return sumIntSlice(maxCals[:])
+}
+
 func buildBackpacks() []backpack {
 	f, _ := os.Open("input/day01.txt")
 	s := bufio.NewScanner(f)
@@ -30,18 +58,4 @@ func buildBackpacks() []backpack {
 	}
 
 	return backpacks
-}
-
-func d1p1() int {
-	backpacks := buildBackpacks()
-	maxCals := -1
-
-	for _, bp := range backpacks {
-		calories := sumIntSlice(bp.food)
-		if calories > maxCals {
-			maxCals = calories
-		}
-	}
-
-	return maxCals
 }
